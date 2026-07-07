e941787e4e0f3dbb93270d381533ac1b4972e96c
2004annapurna@gmail.com <2004annapurna@gmail.com>
2026-07-07 10:31:19 +0530

    created  class

--- src/test/java/tests/Dashboardtest.java
+++ src/test/java/tests/Dashboardtest.java
@@ -12,6 +12,5 @@ public class Dashboardtest extends BasePage {
 	public void verifyDashboard() {
 		Assert.assertTrue(dp.checkDashboardHeaderIsdisplayed());
 		Reporter.log("verified dashboard", true);
-
 	}
 }
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void Vertify_Login() {
+		System.out.println("Logged In");
+	}
+
+}
